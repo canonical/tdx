6e2d8fdba68262b973fb28c380c26df9e3d44201
hector <122458375+petitfou@users.noreply.github.com>
2023-11-17 15:33:07 +0100

    Libvirt usage enhancement (#13)
    * Improve libvirt instructions
    
    Create script to allow to run TD with libvirt
    
    * Put login instruction after libvirt instructions
    
    + generate the td_guest.xml from template with image path
    + whitespace cleanup
    
    * run_td_virsh: Add 5s wait for VM to be shutdown

---
+++ guest-tools/run_td_virsh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TD_VM="td_guest"
+
+if [ ! -f "${TD_IMG}" ]; then
+  echo "Please provide image path by setting TD_IMG variable"
+  exit 1
+fi
+
+virsh shutdown ${TD_VM} &> /dev/null
+virsh shutdown --domain ${TD_VM} &> /dev/null
+
+echo "Wait for VM to shutdown ..."
+sleep 5
+
+virsh destroy ${TD_VM} &> /dev/null
+virsh destroy --domain ${TD_VM} &> /dev/null
+
+virsh undefine ${TD_VM} &> /dev/null
+
+# Generate td_guest.xml with the right image path provided by the user
+TD_IMG_PATH=$(realpath ${TD_IMG})
+awk -v img_path=${TD_IMG_PATH} '{gsub("TD_IMG_PATH", img_path, $0); print}' td_guest.xml.template > td_guest.xml
+
+virsh define ${TD_VM}.xml
+virsh start ${TD_VM}
