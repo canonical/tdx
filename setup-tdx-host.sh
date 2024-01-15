cbd32bace512e8984b00691f970a1352c329ff76
Hector Cao <hector.cao@canonical.com>
2024-01-15 10:57:29 +0100

    Improve error handling in host setup script

--- setup-tdx-host.sh
+++ setup-tdx-host.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+_error() {
+		echo "Error : $1"
+		exit 1
+}
+
 apt update
 apt install --yes software-properties-common &> /dev/null
 
@@ -18,8 +23,8 @@ EOF
 apt update
 
 # --allow-downgrades : if kobuk-tdx-host is already installed
-apt install --yes --allow-downgrades kobuk-tdx-host
-apt upgrade --yes --allow-downgrades kobuk-tdx-host
+apt install --yes --allow-downgrades kobuk-tdx-host || _error "Cannot install TDX components !"
+apt upgrade --yes --allow-downgrades kobuk-tdx-host || _error "Cannot upgrade TDX components !"
 
 # update cmdline to add tdx=1 to kvm_intel
 grep -E "GRUB_CMDLINE_LINUX.*=.*\".*kvm_intel.tdx( )*=1.*\"" /etc/default/grub &> /dev/null
@@ -38,3 +43,6 @@ if [ $? -ne 0 ]; then
   grub-install
 fi
 
+echo "========================================================================"
+echo "The setup has been done successfully. Please enable now TDX in the BIOS."
+echo "========================================================================"
