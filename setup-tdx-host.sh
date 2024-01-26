d625738b12c86052510677b0730e6b34f5ad9411
Hector Cao <hector.cao@canonical.com>
2024-01-26 10:49:34 +0000

    switch to preview kernel 6.7
    disable hibernate because TDX cannot work with S3 hibernate

--- setup-tdx-host.sh
+++ setup-tdx-host.sh
@@ -26,10 +26,27 @@ apt update
 apt install --yes --allow-downgrades kobuk-tdx-host || _error "Cannot install TDX components !"
 apt upgrade --yes --allow-downgrades kobuk-tdx-host || _error "Cannot upgrade TDX components !"
 
+# for now, use the preview kernel in the PPA
+apt install --yes linux-image-unsigned-6.7.0-7-generic
+
 # update cmdline to add tdx=1 to kvm_intel
 grep -E "GRUB_CMDLINE_LINUX.*=.*\".*kvm_intel.tdx( )*=1.*\"" /etc/default/grub &> /dev/null
 if [ $? -ne 0 ]; then
   sed -i -E "s/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 kvm_intel.tdx=1\"/g" /etc/default/grub
+  update-grub
+  grub-install
+fi
+
+# nohibernate
+# TDX cannot survive from S3 and deeper states.  The hardware resets and
+# disables TDX completely when platform goes to S3 and deeper.  Both TDX
+# guests and the TDX module get destroyed permanently.
+# The kernel uses S3 for suspend-to-ram, and use S4 and deeper states for
+# hibernation.  Currently, for simplicity, the kernel chooses to make TDX
+# mutually exclusive with S3 and hibernation.
+grep -E "GRUB_CMDLINE_LINUX.*=.*\".*nohibernate.*\"" /etc/default/grub &> /dev/null
+if [ $? -ne 0 ]; then
+  sed -i -E "s/GRUB_CMDLINE_LINUX=\"(.*)\"/GRUB_CMDLINE_LINUX=\"\1 nohibernate\"/g" /etc/default/grub
   update-grub
   grub-install
 fi
