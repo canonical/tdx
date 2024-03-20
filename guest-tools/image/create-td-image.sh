617ecba567f746cf82af32b02dd786122afa1eba
Hector Cao <hector.cao@canonical.com>
2024-03-20 00:14:37 +0000

    virt-customize needs dhclient on host to have name resolution

--- guest-tools/image/create-td-image.sh
+++ guest-tools/image/create-td-image.sh
@@ -235,6 +235,11 @@ virsh undefine tdx-config-cloud-init &> /dev/null
 echo "Installing required tools ..."
 apt install --yes qemu-utils libguestfs-tools virtinst genisoimage libvirt-daemon-system
 
+# to allow virt-customize to have name resolution, dhclient should be available
+# on the host system. that is because virt-customize will create an appliance (with supermin)
+# from the host system and will collect dhclient into the appliance
+apt install --yes isc-dhcp-client
+
 check_tool qemu-img
 check_tool virt-customize
 check_tool virt-install
