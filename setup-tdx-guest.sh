953b272e2feba61ba98895c5ccdf46974574e39d
Hector Cao <hector.cao@canonical.com>
2024-03-20 03:05:05 +0000

    guest_script: install modules-extra to have tdx_guest

--- setup-tdx-guest.sh
+++ setup-tdx-guest.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+# grub: switch to kernel version
+grub_switch_kernel() {
+    KERNELVER=$1
+    MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
+    KID=$(awk "/with Linux $KERNELVER/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
+    cat > /etc/default/grub.d/99-tdx-kernel.cfg <<EOF
+GRUB_DEFAULT=saved
+GRUB_SAVEDEFAULT=true
+EOF
+    grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
+    update-grub
+}
+
 apt update
 apt install --yes software-properties-common &> /dev/null
 
@@ -16,6 +30,13 @@ Pin-Priority: 4000
 EOF
 
 apt update
+
+# for now, use the generic kernel
+#apt install --allow-downgrades --yes linux-image-unsigned-6.7.0-1001-intel
+#grub_switch_kernel 6.7.0-1001-intel
+
+# install modules-extra to have tdx_guest module
+apt install -y linux-modules-extra-$(uname -r)
 
 # install TDX feature
 apt install -y kobuk-tdx-guest
