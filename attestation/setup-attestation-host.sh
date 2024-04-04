894703da2a8be7dfa9e5db4628e2f00b5e3f63c6
Hector Cao <122458375+hector-cao@users.noreply.github.com>
2024-04-04 16:08:32 +0200

    Merge main-next to main to enable attestation setup (#51)
    * Add attestation setup script
    
    * Check script is run with root privileges
    
    * Add trust authority client in setup guest script
    
    * Add build-essential to compile tdx-attest
    
    * Add remote attestation setup and how to use instruction (#42)
    
    Signed-off-by: Bun K Tan <bun.k.tan@intel.com>
    
    * Validate and clean up attestation instructions (#48)
    
    * Validate and clean up attestation instructions
    
    * Use more explicit naming of attestation scripts
    
    * Remove sudo from commands in guest
    
    * Remove useless script setup-attestation.sh
    
    * Use tdx-release PPA that contains released components (#49)
    
    * Improve attestation setup script (#50)
    
    * setup-attestation-host.sh : do not set user and group for /dev/sgx_provision
    
    the udev rules will take care of it
    
    * Remove README section on use_secure_cert
    
    with the version 1.18-0ubuntu2 of libsgx-dcap-default-qpl
    the use_secure_cert is set to false by default
    
    * Add reboot instruction in README
    
    after the setup script run for attestation, we need to reboot
    to have the right owner & group for /dev/sgx_provision (from udev rule)
    
    ---------
    
    Signed-off-by: Bun K Tan <bun.k.tan@intel.com>
    Co-authored-by: Ubuntu <ubuntu@sysid-739457.maas>
    Co-authored-by: Bun K Tan <bun.k.tan@intel.com>
    Co-authored-by: Will French <will.french@canonical.com>

---
+++ attestation/setup-attestation-host.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt install --yes software-properties-common
+add-apt-repository -y ppa:kobuk-team/tdx-release
+
+apt update
+apt install --yes libsgx-ae-id-enclave \
+    libsgx-ae-qve \
+    libsgx-ae-tdqe \
+    libsgx-dcap-default-qpl \
+    libsgx-dcap-quote-verify \
+    libsgx-pce-logic \
+    libsgx-psw-common \
+    libsgx-ra-uefi \
+    libsgx-tdx-logic \
+    sgx-dcap-pccs \
+    sgx-pck-id-retrieval-tool  \
+    tdx-qgs
+
+# add qgsd user to group  sgx_prv to allow to use /dev sgx
+#ubuntu@sysid-739457:~/tdx$ ls -la /dev/sgx_*
+#crw-rw---- 1 root sgx  10, 125 Nov  8 17:44 /dev/sgx_enclave
+#crw------- 1 root root 10, 126 Nov  8 17:44 /dev/sgx_provision
+#crw-rw---- 1 root sgx  10, 124 Nov  8 17:44 /dev/sgx_vepc
+
+# libsgx-enclave-common/etc/udev/rules.d/94-sgx-enclave.rules
+
+usermod -aG sgx_prv qgsd
+
+
