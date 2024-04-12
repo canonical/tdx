667aad02cdbe5db3833018ef9102e5d0b086e2c5
Hector Cao <122458375+hector-cao@users.noreply.github.com>
2024-04-12 14:40:31 +0200

    Add intel kernel as option that can be selected at guest image creation  (#56)
    * Allow user to use -intel kernel at guest image creation
    
    Use has to set the environment variable TDX_GUEST_SETUP_INTEL_KERNEL at setup-tdx-guest.sh script
    invocation
    
    * Allow user to use -intel kernel at guest image creation
    
    Use has to set the environment variable TDX_GUEST_SETUP_INTEL_KERNEL at setup-tdx-guest.sh script
    invocation
    
    Review #1 : Improvement of the script to be able to go back to generic kernel if
                the guest setup script is run again on a system with -intel kernel enabled

--- setup-tdx-guest.sh
+++ setup-tdx-guest.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# for now, use the generic kernel
-KERNEL_RELEASE=6.8.0-22-generic
+# use can use -intel kernel by setting TDX_GUEST_SETUP_INTEL_KERNEL
+if [ ! -z "${TDX_GUEST_SETUP_INTEL_KERNEL}" ]; then
+   KERNEL_RELEASE=6.8.0-1001-intel
+fi
 
 # grub: switch to kernel version
 grub_switch_kernel() {
@@ -16,6 +18,22 @@ GRUB_SAVEDEFAULT=true
 EOF
     grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
     update-grub
+}
+
+# select the kernel release for next boot
+# if KERNEL_RELEASE is specified, we use it
+# if not, select the latest generic kernel available on the system
+grub_set_kernel() {
+   if [ -z "${KERNEL_RELEASE}" ]; then
+      KERNEL_RELEASE=$(ls /boot/vmlinuz-*-generic 2>&1 | \
+                      /usr/lib/grub/grub-sort-version -r 2>&1 | \
+                      gawk 'match($0 , /^\/boot\/vmlinuz-(.*)/, a) {print a[1];exit}')
+    fi
+    if [ -z "${KERNEL_RELEASE}" ]; then
+      echo "ERROR : unable to determine kernel release"
+      exit 1
+    fi
+    grub_switch_kernel ${KERNEL_RELEASE}
 }
 
 apt update
@@ -33,20 +51,27 @@ Pin: release o=LP-PPA-kobuk-team-tdx
 Pin-Priority: 4000
 EOF
 
-apt update
+# upgrade the system to have the latest components (mostly generic kernel)
+apt upgrade --yes
 
 # install TDX feature
 # install modules-extra to have tdx_guest module
 apt install --yes --allow-downgrades \
-   linux-image-unsigned-${KERNEL_RELEASE} \
-   linux-modules-extra-${KERNEL_RELEASE} \
    shim-signed \
    grub-efi-amd64-signed \
    grub-efi-amd64-bin \
    tdx-tools-guest \
    python3-pytdxmeasure
 
-grub_switch_kernel ${KERNEL_RELEASE}
+# if a specific kernel has to be used
+# TODO : install linux-modules-extra
+if [ ! -z "${KERNEL_RELEASE}" ]; then
+  apt install --yes --allow-downgrades \
+    linux-image-unsigned-${KERNEL_RELEASE}
+fi
+
+# select the right kernel for next boot
+grub_set_kernel
 
 # setup attestation
 ${SCRIPT_DIR}/attestation/setup-guest.sh
