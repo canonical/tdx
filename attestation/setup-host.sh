c3c7a19731a7223128c8e5099650f5bcd23c2396
Hector Cao <hector.cao@canonical.com>
2024-04-22 19:31:03 +0000

    attestation/setup-host.sh : skip adding qgsd to sgx_prv
    the group sgx_prv is created by sgx-setup package
    and tdx-qgs takes care of adding the qgsd user to this group
    no need to do it in this script

--- attestation/setup-host.sh
+++ attestation/setup-host.sh
@@ -18,13 +18,3 @@ apt install --yes sgx-ra-service
 # using indirect registration method
 apt install --yes sgx-pck-id-retrieval-tool
 
-# add qgsd user to group  sgx_prv to allow to use /dev sgx
-#ubuntu@sysid-739457:~/tdx$ ls -la /dev/sgx_*
-#crw-rw---- 1 root sgx  10, 125 Nov  8 17:44 /dev/sgx_enclave
-#crw------- 1 root root 10, 126 Nov  8 17:44 /dev/sgx_provision
-#crw-rw---- 1 root sgx  10, 124 Nov  8 17:44 /dev/sgx_vepc
-
-# /dev/sgx_provision must have sgx_prv as group
-
-usermod -aG sgx_prv qgsd
-
