45bf772682de24427a9041247f3824df426ca7fb
Hector Cao <hector.cao@canonical.com>
2024-06-24 17:15:33 +0200

    update license header based on reviews

--- guest-tools/image/create-td-image.sh
+++ guest-tools/image/create-td-image.sh
@@ -1,9 +1,21 @@
 # This source code is a modified copy of https://github.com/intel/tdx-tools.git
 # See LICENSE.apache file for original license information.
 
-# Copyright (C) 2024 Canonical Ltd.
-#
-# This file is part of tdx repo. See LICENSE file for license information.
+# This file is part of Canonical's TDX repository which includes tools
+# to setup and configure a confidential computing environment
+# based on Intel TDX technology.
+# See the LICENSE file in the repository for the license text.
+
+# Copyright 2024 Canonical Ltd.
+# SPDX-License-Identifier: GPL-3.0-only
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranties
+# of MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
 
 #!/bin/bash
 #
