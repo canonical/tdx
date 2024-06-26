26f6e7d2a8e8f1971701b5b4273d450fddb6329c
Scott McMillan <scott.mcmillan@canonical.com>
2024-06-26 09:47:00 -0400

    PEK 764 additional changes * Use relative checkbox-cli path to be compatible to using other runtime * In pytest, check for image specification and existence * Make sure TDXTEST_GUEST_IMG variable is passed to tox   environments (e.g. checkbox22, checkbox24, ...)

--- tests/tests/pytest_prep.sh
+++ tests/tests/pytest_prep.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-IMAGE_FILE=/var/tmp/tdxtest/tdx-guest.qcow2
+LOCAL_IMG="${SCRIPT_DIR}/../tdx-guest.qcow2"
 
 _error() {
   echo "Error : $1"
@@ -28,6 +27,22 @@ install_deps() {
 
 rm -rf /var/tmp/tdxtest
 mkdir -p /var/tmp/tdxtest
+
+if [ -z "${TDXTEST_GUEST_IMG}" ]; then
+  if ! test -f $LOCAL_IMG; then
+    echo "TDXTEST_GUEST_IMG must be specified!"
+    echo "e.g. export TDXTEST_GUEST_IMG=/tmp/tmp.qcow2"
+    echo "(Use sudo -E to pass environment to sudo)"
+    exit 1
+  fi
+else
+  if ! test -f $TDXTEST_GUEST_IMG; then
+    echo "\$TDXTEST_GUEST_IMG specified, but does not exist!"
+    echo "  Can't find $TDXTEST_GUEST_IMG"
+    exit 1
+  fi
+fi
+
 
 cleanup
 
