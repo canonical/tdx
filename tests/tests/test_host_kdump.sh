134ab39eb51e89af2426cceb14963e23aa8e9ea2
Scott McMillan <scott.mcmillan@canonical.com>
2024-10-01 11:40:46 -0400

    Using set -e and taking in other PR comments

--- tests/tests/test_host_kdump.sh
+++ tests/tests/test_host_kdump.sh
@@ -15,90 +15,64 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+SSH_OPTIONS=" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
 if [ ! -v DEVICE_IP ]; then
     echo "Must define DEVICE_IP"
-    exit -1
+    exit 1
 fi
 
 echo "Installing linux-crashdump and rebooting"
+set -e
 ssh ubuntu@$DEVICE_IP sudo apt install linux-crashdump
-if [ $? != 0 ]; then
-    echo "Can't install linux-crashdump"
-    exit -1
-fi
-
 ssh ubuntu@$DEVICE_IP sudo systemctl reboot
-if [ $? != 0 ]; then
-    echo "Can't reboot"
-    exit -1
-fi
-
 sleep 15
-echo "Waiting for system to come back up"
 
+echo "Waiting for system to come back up"
 cnt=0
 until ssh ubuntu@$DEVICE_IP ls &> /dev/null; do sleep 1; cnt=$(expr $cnt + 1); if [ $cnt -gt 120 ]; then break; fi; done
 if [ $cnt -gt 120 ]; then
     echo "Timed out waiting for $DEVICE_IP to come back up ($cnt)"
-    exit -1
+    exit 1
 fi
 if [ $cnt == 0 ]; then
-    echo "$DEVICE_IP came back too quickly"
-    exit -1
+    echo "$DEVICE_IP came back quickly, likely did not reboot as expected"
+    exit 1
 fi
 
 echo "System came back up, printing out useful debug info"
 
 ssh ubuntu@$DEVICE_IP sudo kdump-config show
-if [ $? != 0 ]; then
-    echo "Failed getting kdump config"
-    exit -1
-fi
-
 ssh ubuntu@$DEVICE_IP cat /proc/cmdline
-if [ $? != 0 ]; then
-    echo "Failed getting cmd line"
-    exit -1
-fi
 
 # Get crash directory before to compare later
 CRASH_DIR_BEFORE="$(ssh ubuntu@$DEVICE_IP ls /var/crash)"
-if [ $? != 0 ]; then
-    echo "Failed getting crash directory"
-    exit -1
-fi
 
 echo "Crashing system"
 ssh ubuntu@$DEVICE_IP "echo c | sudo tee /proc/sysrq-trigger" > /dev/null &
-
 sleep 10
-echo "Waiting for system to come back up"
 
+echo "Waiting for system to come back up"
 cnt=0
 until ssh ubuntu@$DEVICE_IP ls &> /dev/null; do sleep 1; cnt=$(expr $cnt + 1); if [ $cnt -gt 120 ]; then break; fi; done
 if [ $cnt -gt 120 ]; then
     echo "Timed out waiting for $DEVICE_IP to come back up ($cnt)"
-    exit -1
+    exit 1
 fi
 if [ $cnt == 0 ]; then
-    echo "$DEVICE_IP came back too quickly"
-    exit -1
+    echo "$DEVICE_IP came back quickly, likely did not crash as expected"
+    exit 1
 fi
 
 echo "System came back up"
 CRASH_DIR_AFTER="$(ssh ubuntu@$DEVICE_IP ls /var/crash)"
-if [ $? != 0 ]; then
-    echo "Failed getting crash directory"
-    exit -1
-fi
-
 echo Before crash directory listing: $CRASH_DIR_BEFORE
 echo After crash directory listing: $CRASH_DIR_AFTER
 
-# Verifyng crash directory are not the same (extra entry should be in place)
+# Verifying crash directory are not the same (extra entry should be in place)
 if [ "$CRASH_DIR_BEFORE" == "$CRASH_DIR_AFTER" ]; then
     echo "Crash directories are the same"
-    exit -1
+    exit 1
 fi
 
 echo "Kdump Successfully Performed"
