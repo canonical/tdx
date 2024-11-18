canonical/tdx

commit 198e71ae05ecdff5d751f3c4bbf9b19448ee4c95

Noble 24.04 minor improvements (#272)
* tests : qemu : output the qemu command to a script

this will allow users to run manually the guest after the test
this capability is useful for debugging purposes

* tests : qemu : shutdown the VM properly at qemu stop

right now, we use proc.terminate() to send SIGTERM to qemu process
this tends to corrupt the rootfs
this will prevent us from running the guest again with this rootfs
try to stop the VM (shutdown) properly first and terminate() it
later if necessary

tests/lib/Qemu.py
@@ -27,6 +27,7 @@ import subprocess
 import tempfile
 import time
 import sys
+import stat
 
 import util
 
@@ -72,6 +73,17 @@ class QemuGraphic():
         if self.nographic == True:
             return ['-nographic']
         return []
+
+class QemuSerial():
+    def __init__(self, serial_file : str = None):
+        self.serial_file = serial_file
+    def args(self):
+        if self.serial_file:
+            return [
+                '-chardev', f'file,id=c1,path={self.serial_file},signal=off',
+                '-device', 'isa-serial,chardev=c1'
+            ]
+        return ['-serial', 'stdio']
 
 class QemuUserConfig:
     def __init__(self):
@@ -229,6 +241,7 @@ class QemuCommand:
                         'config': QemuUserConfig(),
                         'memory': QemuMemory(memory),
                         'ovmf' : QemuOvmf(machine),
+                        'serial' : QemuSerial(f'{self.workdir}/serial.log'),
                         'machine' : QemuMachineType(machine)}
         self.command = ['-pidfile', f'{self.workdir}/qemu.pid']
 
@@ -237,13 +250,6 @@ class QemuCommand:
         for p in self.plugins.values():
             _args.extend(p.args())
         return _args + self.command
-
-    def add_serial_to_file(self):
-        # serial to file
-        self.command = self.command + [
-            '-chardev', f'file,id=c1,path={self.workdir}/serial.log,signal=off',
-            '-device', 'isa-serial,chardev=c1'
-        ]
 
     def add_qemu_run_log(self):
         # serial to file
@@ -345,6 +351,9 @@ class QemuMonitor():
 
     def wakeup(self):
         self.send_command("system_wakeup")
+
+    def powerdown(self):
+        self.send_command("system_powerdown")
 
     def __del__(self):
         if self.socket is not None:
@@ -467,6 +476,15 @@ class QemuMachineService:
     QEMU_MACHINE_MONITOR = enum.auto()
     QEMU_MACHINE_QMP = enum.auto()
 
+# run script to run the guest
+# this is used for debugging purpose and allow users
+# to run the guest manually
+qemu_run_script = """
+#!/bin/bash
+echo "To connect to the VM : ssh -p {fwd_port} root@localhost"
+{cmd_str}
+"""
+ 
 class QemuMachine:
     debug_enabled = False
     # hold all qemu instances
@@ -498,7 +516,6 @@ class QemuMachine:
             self.fwd_port = util.tcp_port_available()
             self.qcmd.add_port_forward(self.fwd_port)
         self.qcmd.add_qemu_run_log()
-        self.qcmd.add_serial_to_file()
 
         self.proc = None
         self.out = None
@@ -572,6 +589,8 @@ class QemuMachine:
         """
         cmd = self.qcmd.get_command()
         print(' '.join(cmd))
+        script=f'{self.workdir_name}/run.sh'
+        self.write_cmd_to_file(script)
         self.proc = subprocess.Popen(cmd,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
@@ -580,11 +599,7 @@ class QemuMachine:
         """
         Run qemu and wait for its start (by waiting for monitor file's availability)
         """
-        cmd = self.qcmd.get_command()
-        print(' '.join(cmd))
-        self.proc = subprocess.Popen(cmd,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+        self.run()
         QemuMonitor(self)
 
     def communicate(self):
@@ -602,13 +617,28 @@ class QemuMachine:
         """
         if self.proc is None:
             return
-        # self.proc.returncode== None -> not yet terminated
-        if self.proc.returncode is None:
-            try:
-                self.proc.terminate()
-                self.communicate()
-            except Exception as e:
-                print(f'Exception {e}')
+        if self.proc.returncode is not None:
+            return
+
+        # self.proc.returncode == None -> not yet terminated
+
+        # try to shutdown the VM properly, this is important to avoid
+        # rootfs corruption if we want to run the guest again
+        mon = QemuMonitor(self)
+        mon.powerdown()
+        try:
+            self.communicate()
+            return
+        except Exception as e:
+            pass
+
+        print('Qemu process did not shutdown properly, terminate it ...')
+        # terminate qemu process (SIGTERM)
+        try:
+            self.proc.terminate()
+            self.communicate()
+        except Exception as e:
+            print(f'Exception {e}')
 
     def reboot(self):
         """
@@ -622,6 +652,33 @@ class QemuMachine:
         self.communicate()
         # run the VM again
         self.run()
+
+    def write_cmd_to_file(self, fname : str):
+        """
+        Write the qemu command to a executable bash script
+        """
+        # force -serial to stdio to be able to have the console on stdio
+        cur_serial = self.qcmd.plugins['serial']
+        self.qcmd.plugins['serial'] = QemuSerial()
+
+        cmd = self.qcmd.get_command()
+        with open(fname, 'w+') as run_script:
+            cmd_str=''
+            for el in cmd:
+                # escape qemu object with quotes
+                # for example : -object "{'qom-type': 'tdx-guest', 'id': 'tdx'}"
+                if el.startswith('{') and el.endswith('}'):
+                    cmd_str += f'\"{el}\" '
+                else:
+                    cmd_str += f'{el} '
+                script_contents = qemu_run_script.format(fwd_port=self.fwd_port,
+                                                         cmd_str=cmd_str)
+            run_script.write(script_contents)
+        f = pathlib.Path(fname)
+        f.chmod(f.stat().st_mode | stat.S_IEXEC)
+
+        # restore serial config
+        self.qcmd.plugins['serial'] = cur_serial
 
     def __del__(self):
         """

commit f36e38c86aaf84a727ec4e5623243f11644f8ea9

several improvements for tests in Ubuntu 24.10 (#276)
tests :several improvements

* tests : do not display fwd_port since it is not always available

this bug has been introduced in the commit 198e71a

* tests : allow only one instance of monitor per qemu machine

* tests : add prefix to test function to be aligned with test file

* tests : tox : add all tests except perf tests category

* tests : increase ssh timeout for tests

test_stress_huge_resource_vm and test_stress_max_vcpous need more time
for the guest to boot

* tests : tox : ignore tests in tests/guest folder

this folder contains the tests that should be directly executed in the
guest, we have to skip them

* tests : does not make the test fail if we cannot stop correctly the guest

tests/lib/Qemu.py
@@ -299,6 +299,12 @@ class QemuMonitor():
     READ_TIMEOUT = 2
     CONNECT_RETRIES = 60
 
+    def __new__(cls, qemu):
+        # only 1 monitor per qemu machine
+        if qemu.monitor is None:
+            qemu.monitor = super().__new__(cls)
+        return qemu.monitor
+
     def __init__(self, qemu):
         self.socket = None
         assert qemu.qcmd.monitor_file != None, "Monitor socket file is undefined"
@@ -312,11 +318,13 @@ class QemuMonitor():
                 break
             except Exception as e:
                 # give some time to make sure socket file is available
-                print(f'Try to connect to qemu : {qemu.qcmd.monitor_file} : {e}')
                 time.sleep(1)
         self.socket.settimeout(self.READ_TIMEOUT)
+        # wait for prompt
+        print(f'Connected : {qemu.qcmd.monitor_file}, wait for prompt.')
+        self.wait_prompt()
 
-    def recv(self):
+    def recv_data(self):
         msg = ''
         try:
             while True:
@@ -327,6 +335,22 @@ class QemuMonitor():
                 msg += recv_data.decode('utf-8')
         except:
             pass
+        return msg
+
+    def wait_prompt(self):
+        msg = self.recv_data()
+        assert self.DELIMITER_STRING in msg, f'Fail on wait for monitor prompt : {msg}'
+
+    def recv(self):
+        """
+        Return an array of messages from qemu process
+        separated by the prompt string (qemu)
+        Example:
+        (qemu) running
+        (qemu) rebooting
+        will result in the returned value : [' running', ' rebooting']
+        """
+        msg = self.recv_data()
         return msg.split(self.DELIMITER_STRING)
 
     def send_command(self, cmd):
@@ -481,7 +505,6 @@ class QemuMachineService:
 # to run the guest manually
 qemu_run_script = """
 #!/bin/bash
-echo "To connect to the VM : ssh -p {fwd_port} root@localhost"
 {cmd_str}
 """
  
@@ -511,6 +534,10 @@ class QemuMachine:
             )
         self.qcmd.add_image(self.image_path)
         self.qcmd.add_monitor()
+        # monitor client associated to this machine
+        # since there could be only one client, we keep track
+        # of this client instance in the qemu machine object
+        self.monitor = None
         self.qcmd.add_qmp()
         if QemuMachineService.QEMU_MACHINE_PORT_FWD not in service_blacklist:
             self.fwd_port = util.tcp_port_available()
@@ -606,7 +633,7 @@ class QemuMachine:
         """
         Wait for qemu to exit
         """
-        self.out, self.err = self.proc.communicate()
+        self.out, self.err = self.proc.communicate(timeout=60)
         if self.proc.returncode != 0:
             print(self.err.decode())
         return self.out, self.err
@@ -622,17 +649,19 @@ class QemuMachine:
 
         # self.proc.returncode == None -> not yet terminated
 
-        # try to shutdown the VM properly, this is important to avoid
-        # rootfs corruption if we want to run the guest again
-        mon = QemuMonitor(self)
-        mon.powerdown()
         try:
+            # try to shutdown the VM properly, this is important to avoid
+            # rootfs corruption if we want to run the guest again
+            # catch exception and ignore it since we are stopping .... no need to fail the test
+            mon = QemuMonitor(self)
+            mon.powerdown()
+
             self.communicate()
             return
         except Exception as e:
             pass
 
-        print('Qemu process did not shutdown properly, terminate it ...')
+        print(f'Qemu process did not shutdown properly, terminate it ... ({self.workdir_name})')
         # terminate qemu process (SIGTERM)
         try:
             self.proc.terminate()
@@ -671,8 +700,7 @@ class QemuMachine:
                     cmd_str += f'\"{el}\" '
                 else:
                     cmd_str += f'{el} '
-                script_contents = qemu_run_script.format(fwd_port=self.fwd_port,
-                                                         cmd_str=cmd_str)
+                script_contents = qemu_run_script.format(cmd_str=cmd_str)
             run_script.write(script_contents)
         f = pathlib.Path(fname)
         f.chmod(f.stat().st_mode | stat.S_IEXEC)

tests/tests/test_stress_boot.py
@@ -22,7 +22,7 @@ import Qemu
 
 import util
 
-def test_boot():
+def test_stress_boot():
     """
     Boot in loop
     """

tests/tests/test_stress_resources.py
@@ -21,7 +21,7 @@ import multiprocessing
 import Qemu
 import util
 
-def test_huge_resource_vm(qm):
+def test_stress_huge_resource_vm(qm):
     """
     Test huge resources  (Intel Case ID 007)
     """
@@ -34,11 +34,12 @@ def test_huge_resource_vm(qm):
     qm.qcmd.plugins['memory'].memory = '%dG' % (huge_mem_gb)
     qm.run()
 
-    ssh = Qemu.QemuSSH(qm)
+    # huge guest memory -> increase the timeout to give more time to guest to boot
+    ssh = Qemu.QemuSSH(qm, timeout=100)
 
     qm.stop()
 
-def test_memory_limit_resource_vm(qm):
+def test_stress_memory_limit_resource_vm(qm):
     """
     Test memory limit resource  (No Intel Case)
     """
@@ -54,7 +55,7 @@ def test_memory_limit_resource_vm(qm):
     qm.stop()
 
 
-def test_max_vcpus(qm):
+def test_stress_max_vcpus(qm):
     """
     Test max vcpus (No Intel Case ID)
     """
@@ -65,12 +66,12 @@ def test_max_vcpus(qm):
     qm.qcmd.plugins['cpu'].nb_cores = num_cpus
     qm.run()
 
-    ssh = Qemu.QemuSSH(qm)
+    ssh = Qemu.QemuSSH(qm, timeout=100)
 
     qm.stop()
 
 
-def test_max_guests():
+def test_stress_max_guests():
     """
     Test max guests (No Intel Case ID)
     """

tests/tox.ini
@@ -24,36 +24,40 @@ commands_pre =
 
 [testenv:test_guest]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_guest_report.xml -k test_guest
+  python3 -m pytest -s -v --junitxml=test_guest_report.xml --ignore=tests/guest -k test_guest
 
 [testenv:test_host]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_host_report.xml -k test_host
+  python3 -m pytest -s -v --junitxml=test_host_report.xml --ignore=tests/guest -k test_host
 
 [testenv:test_boot]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_boot_report.xml -k test_boot
+  python3 -m pytest -s -v --junitxml=test_boot_report.xml --ignore=tests/guest -k test_boot
 
 [testenv:test_perf]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_perf_report.xml -k test_perf
+  python3 -m pytest -s -v --junitxml=test_perf_report.xml --ignore=tests/guest -k test_perf
 
 [testenv:test_quote]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_quote_report.xml -k test_quote
+  python3 -m pytest -s -v --junitxml=test_quote_report.xml --ignore=tests/guest -k test_quote
 
 [testenv:test_stress]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_stress_report.xml -k test_stress
+  python3 -m pytest -s -v --junitxml=test_stress_report.xml --ignore=tests/guest -k test_stress
+
+[testenv:test_all_except_perf]
+commands =
+  python3 -m pytest -s -v --junitxml=test_all_report.xml --ignore=tests/guest -k 'not test_perf'
 
 [testenv:test_all]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_all_report.xml
+  python3 -m pytest -s -v --junitxml=test_all_report.xml --ignore=tests/guest
 
 [testenv:test_specify]
 commands = 
-  python3 -m pytest -s -v --junitxml=test_specify_report.xml -k {posargs}
+  python3 -m pytest -s -v --junitxml=test_specify_report.xml --ignore=tests/guest -k {posargs}
 
 [testenv:collect_tests]
 commands =
-  python3 -m pytest -s -v --collect-only -k {posargs}
+  python3 -m pytest -s -v --collect-only --ignore=tests/guest -k {posargs}