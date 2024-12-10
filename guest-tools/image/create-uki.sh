595070413fda50e5e8f2a27ab8f99a79bf2b2035
Hector Cao <hector.Cao@canonical.com>
2024-12-10 09:27:38 +0000

    create-uki : more robust boot argument for kernel commandline

--- guest-tools/image/create-uki.sh
+++ guest-tools/image/create-uki.sh
@@ -36,7 +36,12 @@ fi
 echo "Creating the UKI with kernel ${KERNEL_VER}"
 
 # the kernel commandline to put in the UKI
-KERNEL_CMDLINE="console=tty1 console=ttyS0 root=/dev/sda1"
+# the boot is specified using label, on Ubuntu cloud
+# image, the rootfs partition is labelled as cloudimg-rootfs
+# lrwxrwxrwx    1        11 UEFI -> ../../sda15
+# lrwxrwxrwx    1        11 BOOT -> ../../sda13
+# lrwxrwxrwx    1        10 cloudimg-rootfs -> ../../sda1
+KERNEL_CMDLINE="console=tty1 console=ttyS0 root=LABEL=cloudimg-rootfs"
 
 # use systemd-ukify to generate UKI
 sudo apt install -y systemd-ukify systemd-boot-efi
